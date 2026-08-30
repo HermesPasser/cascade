f42a427808e72678b589b3ef465bb50ed27f1ad1
Hermes Passer <dsldouglas@hotmail.com>
2026-08-30 17:33:52 -0300

    Check if the file exists on cache before reprocessing it
    This patch checks if the input given to unzip_file and get_first_file
    already exists on the temp folder and if so, returns it instead
    of recreating. This is only possible since we use deterministic
    names to the files on the cache.
    
    Yes, it used lru_cache on those functions but it won't work if we
    restart the server, which is something that can happen for a number
    of reasons. I'm removing lru_cache also since on my tests where
    i manually delete the cache, the lru_cache ended up returning
    the path that does not exist anymore.

--- src/unzip.py
+++ src/unzip.py
@@ -6,17 +6,23 @@ from zipfile import BadZipFile, ZipFile
 from temp import temp_name_for
 
 
-@lru_cache
 def unzip_file(file: str):
-    directory = str(temp_name_for(file))
+    temp = temp_name_for(file)
+    directory = str(temp)
+    if temp.exists():
+        return directory
+
     with ZipFile(file, "r") as archive:
         archive.extractall(directory)
     return directory
 
 
-@lru_cache
 def get_first_file(file: str | os.PathLike, filter_fn: Callable[[str], bool]):
-    directory = str(temp_name_for(file))
+    temp = temp_name_for(file)
+    directory = str(temp)
+    if temp.exists():
+        return directory
+
     try:
         with ZipFile(file, "r") as archive:
             it = filter(filter_fn, sorted((f.filename for f in archive.filelist)))
